e99eefc8b92b4785aa319f672f134e4ce1e1b3cf
Herlian <herlianzhang370@gmail.com>
2019-12-21 13:40:16 +0700

    change

---
+++ lib/dummy_data.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const dummyCategories = const [
+  Category(id: 'c1', title: 'Italian'),
+  Category(id: 'c2', title: 'Indonesian', color: Colors.blue),
+  Category(id: 'c3', title: 'Malaysia'),
+  Category(id: 'c3', title: 'Thailand'),
+  Category(id: 'c4', title: 'China'),
+  Category(id: 'c5', title: 'Amric'),
+  Category(id: 'c6', title: 'Tiongkok'),
+];
